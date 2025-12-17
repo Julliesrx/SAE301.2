dd453066af48b039d21afab91f162ee97bcbc1b2
julz <juliecamille37270@gmail.com>
2025-12-17 22:44:02 +0100

    footer final

--- templates/footer.php
+++ templates/footer.php
@@ -1,5 +1,5 @@
 </div> <footer class="bg-white text-center py-4 mt-5 border-top">
-    <p class="text-muted mb-0">© 2025 Free Message - Projet Étudiant</p>
+    <p class="text-muted mb-0">© 2025 Free Message - SAE 301.2 <a href="https://juliesirieix.com">Julie Sirieix</a></p>
 </footer>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
